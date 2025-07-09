b06affd85ad2cdb5480f41169bf8c0741f0bcdf4
DekayDeCanela <diegoszm11@gmail.com>
2025-07-09 18:55:52 -0500

    Update language-switcher.component.vue

--- src/public/language-switcher.component.vue
+++ src/public/language-switcher.component.vue
@@ -1,1 +1,21 @@
+<script>
+export default {
+  data(){
+    return {
+      languages: ['en', 'es']
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-select-button
+      v-model="$i18n.locale"
+      :options="languages"
+      class="uppercase language-switcher"
+  />
+</template>
+<style>
+
+</style>
 
