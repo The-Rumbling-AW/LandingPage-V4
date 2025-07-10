e6ea98e3d3584f1181bef2db46c6421b4a04fb7d
Antuanete01 <u202310358@upc.edu.pe>
2025-07-09 23:29:12 -0500

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,1 @@
-import en from './locals/en.json'
-import es from './locals/es.json'
 
-import { createI18n } from 'vue-i18n'
-const i18n = createI18n({
-    locale: 'es',
-    messages: {
-        en,
-        es
-    },
-    warnHtmlMessage: false
-})
-export default i18n;
